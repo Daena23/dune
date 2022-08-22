6163839d70418a00f7bff92fe430b85b7f37477f
Natalia Naumenko <lunata9@yandex.ru>
2022-08-22 15:10:18 +0300

    I just removed my old versions from stable branch.

--- src/dune/old_versions/123.py
+++
@@ -1,9 +0,0 @@
-import random
-
-lines = []
-x = 0
-qu = 5
-for x in range(qu):
-    for i in range(qu):
-        lines.append([0] * qu)
-        print(lines[x])
--- src/dune/old_versions/test.py
+++
@@ -1,6 +0,0 @@
-
-explosible_objects = [0, 2, 3, 4, 9]
-i = 91
-if i in explosible_objects:
-    print('egavga')
-
